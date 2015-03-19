bbf1e7501c70f90843b92da46fe19b5dff270649
Oleg Balykin <ezoterik.h@gmail.com>
2015-03-19 02:20:32 +0200

    Обновлен конфиг console

--- config/console.php
+++ config/console.php
@@ -8,10 +8,9 @@ $db = require(__DIR__ . '/db.php');
 return [
     'id' => 'ping-logger-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'gii'],
+    'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
     'modules' => [
-        'gii' => 'yii\gii\Module',
     ],
     'components' => [
         'cache' => [
